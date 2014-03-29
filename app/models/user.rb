760673a602e87375b4ecd15eea9dde917aad72e7
Jake <jaker@Jakes-MacBook.local>
2014-03-29 15:31:31 -0400

    add oauth user model logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |u|
+      u.name = auth.info.name
+      u.uid = auth.uid
+      u.oauth_token = auth.credentials.token
+      u.provider = auth.provider
+      u.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      u.save!
+    end
+  end
+
 end
