1aaa3e524f31c8ece6bcb07df9f081ed399f9206
Jake <jaker@Jakes-MacBook.local>
2014-03-29 14:37:20 -0400

    add rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieM::Application.config.secret_key_base = '4a47b69882a6bdd32e0fd0503865e782ec51ab91eff9be1dbfd50ce3f48cc2c18e8a651076fac39e740fdb805821deeaccef056446d4f626f217879205cce818'
