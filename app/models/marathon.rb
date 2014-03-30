15823e474e890ac6ebb3e5f749292eb2355a49ee
Jake <jaker@Jakes-MacBook.local>
2014-03-30 12:32:39 -0400

    add validations

--- app/models/marathon.rb
+++ app/models/marathon.rb
@@ -1,4 +1,7 @@
 class Marathon < ActiveRecord::Base
 	has_many :chosenmovies
 	belongs_to :user
+
+	validates :name, presence: true
+	validates :duration, numericality: {only_integer: true}
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ActiveRecord::Base
 	has_many :chosenmovies
 	has_many :likedmovies
+
+	validates :name, presence: true
+	validates :runtime, numericality: {only_integer: true}
 end
