73e614390e5abe235e56e3cae3c862b7697f3b5a
Jake <jaker@Jakes-MacBook.local>
2014-03-30 14:47:37 -0400

    fb ready to go

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FB_KEY'], ENV['FB_SECRET']
+  provider :facebook, '263165513850572', '7e09d736fc59d46bd7e12b52faf1cdd4'
 end
