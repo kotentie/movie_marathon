71b4e86c1eb16de66cecb40d6c6c80036e3ef86d
Jake <jaker@Jakes-MacBook.local>
2014-04-03 14:47:29 -0400

    add views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+  	if session[:user_id]
+  	@marathons = Marathon.where(user_id: session[:user_id])
+  	else
+  	@marathons
+  	end
   end
 end
